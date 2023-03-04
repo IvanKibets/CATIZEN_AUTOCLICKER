acdf0437c93268296d50fee69ce1ae8f90ddce86
MidhunNk <midhunmike2023@gmail.com>
2023-03-04 23:52:35 +0530

    Commit Exp21

---
+++ Quick Sort.c
@@ -0,0 +1,68 @@
+// Coded by MidhunMike
+#include <stdio.h>
+
+// Function to swap
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Function to partition
+int partition(int array[], int low, int high)
+{
+
+    int pivot = array[high];
+
+    int i = (low - 1);
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+// Function for perform Quicksort
+void quickSort(int array[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+// Funtion to Display elements
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+// Main Function
+int main()
+{
+    int data[100], i, n;
+    printf("Enter the limit of the array :");
+    scanf("%d", &n);
+    printf("Enter the elements :");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &data[i]);
+    }
+    printf(" array \n");
+    printArray(data, n);
+    quickSort(data, 0, n - 1);
+    printf("Sorted array in ascending order: \n");
+    printArray(data, n);
+}
